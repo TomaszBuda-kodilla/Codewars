79e44d92b00e2607aae07e14ccd07507c22fc3d2
leetdco2@gmail.com <leetdco2@gmail.com>
2020-11-21 13:01:45 +0100

    3.6.1 - 6kyu with binary

--- src/main/java/SumOf.java
+++ src/main/java/SumOf.java
@@ -1,5 +1,29 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/* RULES: The numbers (a,b) may be positive , negative values or zeros .
+Returning value will be an integer .
+Java: the following methods are prohibited:
+    addExact,
+    average,
+    collect,
+    decrement,
+    increment,
+    nextAfter,
+    nextDown,
+    nextUp,
+    reduce,
+    subtractExact,
+    sum,
+    summing . The following classes are prohibited: BigDecimal and BigInteger .*/
 public class SumOf {
     public static int sum(int a, int b) {
-        return Integer.sum(a,b);
+        int result = 0;
+        while (b != 0) {
+            result = a & b;
+            a = a ^ b;
+            b = result << 1;
+        }
+        return a;
     }
 }
