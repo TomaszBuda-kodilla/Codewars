9656d6e7ad4f8a946f664abbfcf93030ed838327
leetdco2@gmail.com <leetdco2@gmail.com>
2020-12-08 08:41:59 +0100

    6kyu SumParts after refactoring + tests + comment

--- src/main/java/SumParts.java
+++ src/main/java/SumParts.java
@@ -1,19 +1,34 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class SumParts {
     public static int[] sumParts(int[] ls) {
-        int[] newTab = new int[ls.length + 1];
-        for (int i = 0; i < newTab.length; i++) {
-            for (int j = 0; j < ls.length; j++) {
-                newTab[i] = newTab[i] + ls[j];
-            }
-            if (i >= ls.length) {
-                newTab[i] = 0;
-            } else {
-                ls[i] = 0;
-            }
+        LinkedList linkedList = new LinkedList();
+        int result = IntStream.of(ls).sum();
+        linkedList.add(result);
+        for (int j = 1; j < ls.length; j++) {
+            linkedList.add(result - ls[j - 1]);
+            result -= ls[j - 1];
         }
-        System.out.println("Elements of array:" + Arrays.toString(newTab));
-        return newTab;
+        if (ls.length > 0) linkedList.add(0);
+        Object[] objectArray = linkedList.toArray();
+        int[] intsArray = new int[objectArray.length];
+        for (int i = 0; i < objectArray.length; i++) {
+            intsArray[i] = (int) objectArray[i];
+        }
+        return intsArray;
     }
 }
+
+/* Other example of this method using stream
+
+public static int[] sumParts(int[] ls) {
+        int[] result = new int[ls.length + 1];
+        int sum = Arrays.stream(ls).sum();
+        for (int i = 0; i<ls.length;i++){
+            result[i] = sum;
+            sum = sum - ls[i];
+        }
+        return result;
+    }
+ */
