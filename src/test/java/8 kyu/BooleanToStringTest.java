a0cd6a53ba3b4e338423eb6b47d5eef24319198f
leetdco2@gmail.com <leetdco2@gmail.com>
2021-01-07 16:28:52 +0100

    BooleanToString with short IF

---
+++ src/test/java/8 kyu/BooleanToStringTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BooleanToStringTest {
+    @Test
+    public void testTrue()
+    {
+        assertEquals(BooleanToString.convert(true), "true");
+    }
+
+    @Test
+    public void testFalse()
+    {
+        assertEquals(BooleanToString.convert(false), "false");
+    }
+}
